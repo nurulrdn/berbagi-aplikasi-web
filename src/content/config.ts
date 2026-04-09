827f03f266ae6b1c3c296508cd5b22faed00e94f
nurulrdn <nurul10ramadhan@gmail.com>
2026-04-09 16:24:09 +0700

    Mematikan total schema Astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,7 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from 'astro:content';
 
 const aplikasiCollection = defineCollection({
-    type: 'content',
-    // z.object() dengan catchall(z.any()) akan menerima semua format tanpa banyak protes
-    schema: z.object({
-        title: z.string().optional().default('Aplikasi Baru'),
-        kategori: z.string().optional(),
-        os: z.string().optional(),
-        ukuran: z.string().optional(),
-        gambar: z.string().optional(),
-        ringkasan: z.string().optional(),
-        
-        // Menggunakan z.any() agar tidak error jika dikosongkan di Ruang Admin
-        screenshots: z.any().optional(),
-        daftar_download: z.any().optional(),
-        
-    }).catchall(z.any()) // 🚀 KUNCI RAHASIA: Terima data apapun yang dikirim CMS
+    type: 'content'
 });
 
 export const collections = {
